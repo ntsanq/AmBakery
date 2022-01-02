9c563445d08c0f4fc13c8f4eebc4b68f835eaeab
ntsanq <nguyenthanhsang2401@gmail.com>
2022-01-02 13:51:52 +0700

    udpate: sua file connect

--- admin/connect.php
+++ admin/connect.php
@@ -1,4 +1,8 @@
 <?php
 
 $connect = mysqli_connect('localhost', 'root','', 'j2school_project');
+if (mysqli_connect_errno()){
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
 mysqli_set_charset($connect,"utf8");
