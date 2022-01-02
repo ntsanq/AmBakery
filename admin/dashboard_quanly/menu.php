9f28006f74debeee5d6dc04edb3b7093cdd1b8d9
ntsanq <nguyenthanhsang2401@gmail.com>
2022-01-03 06:22:42 +0700

    fix: sap xep lai thu muc

--- admin/dashboard_quanly/menu.php
+++ admin/dashboard_quanly/menu.php
@@ -1,43 +1,44 @@
+<!-- https://stackoverflow.com/questions/36004291/how-create-base-url-of-website-if-the-menu-are-in-different-folder -->
 <div class="sidebar">
 	<ul class="nav__list">
 		<li class="nav__list--item">
-			<a href="index.php">
+			<a href="http://localhost/j2school_Project/admin/dashboard_quanly">
 				<i class="fas fa-home nav__icon"></i>
 				<span>Trang chủ</span>
 			</a>
 		</li>
 		<li class="nav__list--item">
-			<a href="nhanvien">
+			<a href="http://localhost/j2school_Project/admin/dashboard_quanly/nhanvien">
 				<i class="fas fa-male nav__icon"></i>
 				<span>Quản lý nhân viên</span>
 			</a>
 		</li>
 		<li class="nav__list--item">
-			<a href="sanpham">
+			<a href="http://localhost/j2school_Project/admin/dashboard_quanly/sanpham">
 				<i class="fas fa-bread-slice nav__icon"></i>
 				<span>Quản lý sản phẩm</span>
 			</a>
 		</li>
 		<li class="nav__list--item">
-			<a href="donhang">
+			<a href="http://localhost/j2school_Project/admin/dashboard_quanly/donhang">
 				<i class="fas fa-shopping-bag nav__icon"></i>
 				<span>Quản lý đơn hàng</span>
 			</a>
 		</li>
 		<li class="nav__list--item">
-			<a href="theloai">
+			<a href="http://localhost/j2school_Project/admin/dashboard_quanly/theloai">
 				<i class="fas fa-tasks nav__icon"></i>
 				<span>Danh mục thể loại</span>
 			</a>
 		</li>
 		<li class="nav__list--item">
-			<a href="khachhang">
+			<a href="http://localhost/j2school_Project/admin/dashboard_quanly/khachhang">
 				<i class="fas fa-users nav__icon"></i>
 				<span>Khách hàng</span>
 			</a>
 		</li>
 		<li class="nav__list--item">
-			<a href="../login.php">
+			<a href="http://localhost/j2school_Project/admin/login.php">
 				<i class="fas fa-sign-out-alt nav__icon"></i>
 				<span>Log out</span>
 			</a>
