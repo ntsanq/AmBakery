a3c182202e6bce2e1697458b1c87883fe4444fd7
ntsanq <nguyenthanhsang2401@gmail.com>
2022-01-21 16:40:11 +0700

    fix: chinh sua css

--- admin/dashboard_quanly/nhanvien/index.php
+++ admin/dashboard_quanly/nhanvien/index.php
@@ -33,89 +33,15 @@
 				$result= mysqli_query($connect, $sql);
 				?>
 
-				<!-- -------------------------Bảng Nhân viên ---------------------->
-				<h2 class="main_content_the_loai">Nhân viên</h2>
-				<table border="1" width="100%">
-					<?php 
-					include  '../../connect.php' ;
-					$sql = "select * from nhan_vien where cap_do = 0 ";
-	  				$result = mysqli_query($connect, $sql);
-					?>
-					<thead class="thead-dark">
-						<tr>
-							<th>
-								Mã
-							</th>
-							<th>
-								Tên nhân viên
-							</th>
-							<th>
-								Địa chỉ
-							</th>
-							<th>
-								Sđt
-							</th>
-							<th>
-								Email
-							</th>
-							<th>
-								Chức vụ
-							</th>
-							<th>
-								Sửa
-							</th>
-							<th>
-								Xóa
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-						<?php foreach ($result as $tung_nhan_vien  ) {?>
-							<tr>
-								<td>
-									<?php echo $tung_nhan_vien['ma'] ?>
-								</td>
-								<td>
-									<?php echo $tung_nhan_vien['ten'] ?>
-								</td>
-								<td>
-									<?php echo $tung_nhan_vien['dia_chi'] ?>
-								</td>
-								<td>
-									<?php echo $tung_nhan_vien['sdt'] ?>
-								</td>
-								<td>
-									<?php echo $tung_nhan_vien['email'] ?>
-								</td>
-								<td>
-									<?php 
-									if ($tung_nhan_vien['cap_do'] == 1) {
-										echo "Quản lý";
-									}else{
-										echo "Nhân viên";
-									}
-									?>
-								</td>
-								<td>
-									<a href="sua_form.php?ma=<?php echo $tung_nhan_vien['ma']?>">Sửa</a>
-								</td>
-								<td>
-									<a href="xoa_process.php?ma=<?php echo $tung_nhan_vien['ma']?>" onclick="if(confirm('Xóa thật hả?')){}else{return false;}"
-										>Xóa</a>
+				
 
-									</td>
-								</tr>
-							<?php } ?>
-						</tbody>
-					</table>
-
-					<!-- -------------------------Bảng Quản lý ---------------------->
+				<!-- -------------------------Bảng Quản lý ---------------------->
 				<h2 class="main_content_the_loai">Quản lý</h2>
 				<table border="1" width="100%">
 					<?php 
 					include  '../../connect.php' ;
 					$sql = "select * from nhan_vien where cap_do = 1 ";
-	  				$result = mysqli_query($connect, $sql);
+					$result = mysqli_query($connect, $sql);
 					?>
 					<thead class="thead-dark">
 						<tr>
@@ -185,9 +111,84 @@
 						</tbody>
 					</table>
 
-				</div>
-			</main>
-		</div>
+					<!-- -------------------------Bảng Nhân viên ---------------------->
+					<h2 class="main_content_the_loai">Nhân viên</h2>
+					<table border="1" width="100%">
+						<?php 
+						include  '../../connect.php' ;
+						$sql = "select * from nhan_vien where cap_do = 0 ";
+						$result = mysqli_query($connect, $sql);
+						?>
+						<thead class="thead-dark">
+							<tr>
+								<th>
+									Mã
+								</th>
+								<th>
+									Tên nhân viên
+								</th>
+								<th>
+									Địa chỉ
+								</th>
+								<th>
+									Sđt
+								</th>
+								<th>
+									Email
+								</th>
+								<th>
+									Chức vụ
+								</th>
+								<th>
+									Sửa
+								</th>
+								<th>
+									Xóa
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php foreach ($result as $tung_nhan_vien  ) {?>
+								<tr>
+									<td>
+										<?php echo $tung_nhan_vien['ma'] ?>
+									</td>
+									<td>
+										<?php echo $tung_nhan_vien['ten'] ?>
+									</td>
+									<td>
+										<?php echo $tung_nhan_vien['dia_chi'] ?>
+									</td>
+									<td>
+										<?php echo $tung_nhan_vien['sdt'] ?>
+									</td>
+									<td>
+										<?php echo $tung_nhan_vien['email'] ?>
+									</td>
+									<td>
+										<?php 
+										if ($tung_nhan_vien['cap_do'] == 1) {
+											echo "Quản lý";
+										}else{
+											echo "Nhân viên";
+										}
+										?>
+									</td>
+									<td>
+										<a href="sua_form.php?ma=<?php echo $tung_nhan_vien['ma']?>">Sửa</a>
+									</td>
+									<td>
+										<a href="xoa_process.php?ma=<?php echo $tung_nhan_vien['ma']?>" onclick="if(confirm('Xóa thật hả?')){}else{return false;}"
+											>Xóa</a>
 
-	</body>
-	</html>
+										</td>
+									</tr>
+								<?php } ?>
+							</tbody>
+						</table>
+					</div>
+				</main>
+			</div>
+
+		</body>
+		</html>
